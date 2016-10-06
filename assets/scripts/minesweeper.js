5c70cea5becf1dac8cb2f23486feab32270ef25a
Pwl Webdev <pwl.webdev@gmail.com>
2016-10-06 20:14:48 +0200

    Mine generation and number calculation completed

--- assets/scripts/minesweeper.js
+++ assets/scripts/minesweeper.js
@@ -2,6 +2,10 @@ $(document).ready(function(){
 	console.log("ready");
 	var board = createBoard();
 	render(board);
+	setMines(board);
+	render(board);
+	setDistances(board);
+	render(board);
 });
 
 var size = 9;
@@ -25,3 +29,59 @@ var render = function(board){
 		}
 	}
 };
+
+var setMines = function(board){
+	var mineNo = 10;
+	do{
+		var col = Math.floor(Math.random()*(size-1));
+		var row = Math.floor(Math.random()*(size-1));
+		if(board[row][col] == " "){
+			board[row][col] = "X";
+			mineNo -= 1;
+		}
+	}while(mineNo > 0)
+}
+
+var setDistances = function(board){
+	var dist = 0;
+	for(var i = 0; i < size; i++){
+		for(var j = 0; j < size; j++){
+			if((i+1) < size && (j+1) < size){
+			 if(board[i+1][j+1] == "X"){
+				dist += 1;}
+			}
+			if((i+1) < size){
+				if(board[i+1][j] == "X"){
+				dist += 1;}
+			}
+			if((i+1) < size && (j-1) >= 0){
+				if(board[i+1][j-1] == "X"){
+				dist += 1;}
+			}
+			if((j+1) < size ){
+				if(board[i][j+1] == "X"){
+				dist += 1;}
+			}
+			if((j-1) >= 0){
+				if(board[i][j-1] == "X"){
+				dist += 1;}
+			}
+			if((i-1) >= 0 && (j+1) < size){
+				if(board[i-1][j+1] == "X"){
+				dist += 1;}
+			}
+			if((i-1) >= 0){
+				if(board[i-1][j] == "X"){
+				dist += 1;}
+			}
+			if((i-1) >= 0 && (j-1) >= 0){
+				if(board[i-1][j-1] == "X"){
+				dist += 1;}
+			}
+			if(dist > 0 && board[i][j] == " "){
+				board[i][j] = dist;
+			}
+			dist = 0;
+		}
+	}
+}
